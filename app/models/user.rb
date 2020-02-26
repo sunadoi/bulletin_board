bad8b535a888e265449b2b65bc210a3f1b946315
sunadoi <y.sunadoi@gmail.com>
2020-02-27 01:11:18 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :topics
+  has_many :responses
+
   validates :nickname, presence: true
 end
