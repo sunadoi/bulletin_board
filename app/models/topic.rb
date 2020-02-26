2b8fa78eda3583d791668778dcffca29f926dd4a
sunadoi <y.sunadoi@gmail.com>
2020-02-26 23:51:15 +0900

    Update topic.rb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,9 @@
 class Topic < ApplicationRecord
   belongs_to :user
   has_many :responses
+  accepts_nested_attributes_for :responses, allow_destroy: true
   has_many :topics_categories
   has_many :categories, through: :topics_categories
+
+  validates :title, presence: true
 end
