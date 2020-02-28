c02809c3f6c94c245c134180702b9e89c8e75989
sunadoi <y.sunadoi@gmail.com>
2020-02-28 19:08:45 +0900

    Update models

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,6 @@
 class Response < ApplicationRecord
-  belongs_to :topic
-  belongs_to :user
+  belongs_to :topic, optional: true
+  belongs_to :user, optional: true
 
   validates :content, presence: true
 end
