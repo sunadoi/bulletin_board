0874d819992f39ddef2644852da46a749bddc4fa
sunadoi <y.sunadoi@gmail.com>
2020-02-28 11:18:09 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def ensure_login
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
